12061e8c82b8c0b40d01509ed0456b291eade7b8
IT-Admin <kenta86burn@gmail.com>
2021-11-15 15:04:52 +0700

    membuat file perulangan

---
+++ looping.py
@@ -0,0 +1,18 @@
+"""
+Perulangan menggunakan while dan for
+"""
+
+
+def main():
+    # menentukan banyak pengulangan
+    n = 5
+
+    # melakukan pengulangan
+    i = 1
+    while i <= n:
+        print(i)
+        i += 1  # atau : i = i + 1
+
+
+if __name__ == "__main__":
+    main()
