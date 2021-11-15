506fc01d208a1e677212b8f82bc9313c9d7626ae
IT-Admin <kenta86burn@gmail.com>
2021-11-15 16:34:20 +0700

    create looping - for n virtual environment

---
+++ looping - for.py
@@ -0,0 +1,18 @@
+"""
+Perulangan menggunakan for
+"""
+
+
+def main():
+    # menentukan banyak pengulangan
+    n = 5
+
+    # melakukan pengulangan
+    i = 1
+    while i <= n:
+        print(i)
+        i += 1  # atau : i = i + 1
+
+
+if __name__ == "__main__":
+    main()
