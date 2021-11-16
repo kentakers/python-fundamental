34f6d5b056528b9ce994fcad089e5a2dec807808
IT-Admin <kenta86burn@gmail.com>
2021-11-16 10:36:49 +0700

    Merapikan file

---
+++ Percabangan/cek_umur.py
@@ -0,0 +1,16 @@
+def main():
+    # cek umur
+    # menggunakan struktur percabangan if dan else
+
+    umur = float(input("berapa umur kamu: "))
+
+    if umur >= 18:
+        print("Selamat anda boleh membuat SIM")
+
+    else:
+        print("maaf anda belum boleh membuat SIM")
+        print("dasar bocah")
+
+
+if __name__ == '__main__':
+    main()
